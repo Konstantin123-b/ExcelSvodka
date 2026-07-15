f1925f854007175a8445937b9affb9e2452cc755
Konstantin123-b <74186429+Konstantin123-b@users.noreply.github.com>
2026-07-15 23:46:37 +0700

    Update run_gui.py

--- run_gui.py
+++ run_gui.py
@@ -1,47 +1,24 @@
 import sys
-import traceback
 
-from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtWidgets import QApplication
 
 from gui.main_window import MainWindow
-
-
-def exception_hook(exc_type, exc_value, exc_traceback):
-    """
-    Глобальный обработчик необработанных исключений.
-    """
-
-    text = "".join(
-        traceback.format_exception(
-            exc_type,
-            exc_value,
-            exc_traceback,
-        )
-    )
-
-    print(text)
-
-    QMessageBox.critical(
-        None,
-        "Критическая ошибка",
-        text,
-    )
 
 
 def main():
 
     app = QApplication(sys.argv)
 
-    app.setApplicationName("ExcelSvodka")
-    app.setOrganizationName("ExcelSvodka")
-
-    sys.excepthook = exception_hook
-
     window = MainWindow()
+
     window.show()
 
-    sys.exit(app.exec())
+    sys.exit(
+        app.exec()
+    )
 
 
 if __name__ == "__main__":
+
     main()
+    
